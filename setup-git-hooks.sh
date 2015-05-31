94935f5272619fa0ba7bd5457176677660b44300
Tiago Royer <royertiago@gmail.com>
2015-05-30 22:49:33 -0300

    setup-git-hooks.sh: Não instalar arquivos com o carater '.'.

--- setup-git-hooks.sh
+++ setup-git-hooks.sh
@@ -5,11 +5,16 @@ installed_hooks=0
 
 for file in githooks/*; do
     file=${file#githooks/}
+    if [[ $file == *.* ]]; then
+        # Special exception: if file contains a dot, then it is not a hook.
+        # This allows us to spread hooks through several .sh files.
+        continue
+    fi
     if [ -e ".git/hooks/$file" ]; then
-        echo Skipping existing hook "$file".
+        echo Skipping existing hook \""$file"\".
     else
         ln -s "../../githooks/$file" .git/hooks/
-        echo Installed hook "$file" using symlink.
+        echo Installed hook \""$file"\" using symlink.
         ((installed_hooks++))
     fi
 done
